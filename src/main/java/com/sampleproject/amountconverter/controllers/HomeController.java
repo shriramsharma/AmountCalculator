9ac67084114ee2d917de0c58b363eee88c49873d
shriramsharma <shriram.sharma22@gmail.com>
2014-07-15 17:04:26 -0400

    Refactored the controller code. * Removed unwanted method. * Renamed the home method to convert.

--- src/main/java/com/sampleproject/amountconverter/controllers/HomeController.java
+++ src/main/java/com/sampleproject/amountconverter/controllers/HomeController.java
@@ -1,19 +1,12 @@
 package com.sampleproject.amountconverter.controllers;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sampleproject.amountconverter.service.IConverter;
@@ -23,36 +16,19 @@ import com.sampleproject.amountconverter.service.IConverter;
  */
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private IConverter converter;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
+
 	@RequestMapping(value = "/convert/{num:.+}", method = RequestMethod.GET)
 	@ResponseBody
-	public String home(@PathVariable String num) {
+	public String convert(@PathVariable String num) {
 		String result = "";
-		if(num == null)
+		if (num == null)
 			return "Invalid param";
 		try {
-			result= converter.toWords(Double.valueOf(num));
+			result = converter.toWords(Double.valueOf(num));
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			e.printStackTrace();
@@ -60,5 +36,5 @@ public class HomeController {
 		}
 		return result;
 	}
-	
+
 }
